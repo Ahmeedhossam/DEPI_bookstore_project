92a9b61a52dcd98b301eaa73bd50b8321bd7048b
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2025-11-25 10:41:17 +0200

    project initialization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DEPI_bookstore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
